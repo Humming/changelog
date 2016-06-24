c7a842471e30319adba621c160b74fec1cfc08cf
Eric Stenseth <eric.stenseth@gmail.com>
2016-06-24 17:40:17 +0200

    got angular2 bundle to work

--- src/Changelog/wwwroot/scripts/app.js
+++ src/Changelog/wwwroot/scripts/app.js
@@ -1,6 +1,24 @@
-function sayHello() {
-    const compiler = document.getElementById("compiler").value;
-    const framework = document.getElementById("framework").value;
-    return `Hello from ${compiler} and ${framework}!`;
-}
-//# sourceMappingURL=app.js.map
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const core_1 = require("angular2/core");
+const model_1 = require("./model");
+let AppComponent = class AppComponent {
+    constructor() {
+        this.model = new model_1.MyModel();
+    }
+    getCompiler() {
+        return this.model.compiler;
+    }
+};
+AppComponent = __decorate([
+    core_1.Component({
+        selector: `my-app`,
+        template: `<div>Hello from {{getCompiler()}}</div>`
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
---
+++ src/Changelog/wwwroot/scripts/model.js
@@ -0,0 +1,7 @@
+"use strict";
+class MyModel {
+    constructor() {
+        this.compiler = "TypeScript";
+    }
+}
+exports.MyModel = MyModel;
