12fee44d0409ee045dc2ade584d9e53183884023
Eric Stenseth <eric.stenseth@gmail.com>
2016-08-26 18:54:38 +0200

    rc5, added ngmodule

---
+++ src/Changelog/scripts/app.module.ts
@@ -0,0 +1,24 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppComponent }  from "./app.component";
+import { ChangeLogFormComponent } from "./changelog-form.component";
+import { ChangeLogListComponent } from "./changelog-list.component";
+import "./rxjs-operators";
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        ChangeLogFormComponent,
+        ChangeLogListComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/Changelog/scripts/main.ts
+++ src/Changelog/scripts/main.ts
@@ -1,7 +1,5 @@
 ///<reference path="./../typings/globals/es6-shim/index.d.ts"/>
-import {bootstrap} from "@angular/platform-browser-dynamic";
-import {HTTP_PROVIDERS} from "@angular/http";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-import {AppComponent} from "./app";
-
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+platformBrowserDynamic().bootstrapModule(AppModule);
