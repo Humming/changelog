89d689f3591567b659f54419169f48bd480dadf3
Eric Stenseth <eric.stenseth@gmail.com>
2017-01-27 13:49:06 +0100

    upgraded to 1.1.0 added xunit

---
+++ tests/ClassLibrary2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        [Fact]
+        public async void FactCheck()
+        {
+            Action a = () => { throw new Exception("!"); };
+            Assert.Throws<Exception>(a);
+
+            Func<Task> testCode = () => { throw new ArgumentException("alex er sulten'ish"); };
+
+            var b = await Assert.ThrowsAsync<ArgumentException>(testCode);
+
+            Assert.Equal("ALEX ER SULTEN",b.Message);
+        }
+    }
+}
