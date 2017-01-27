9f7503b2f1145f937db124274cbeecb48b7d6841
Eric Stenseth <eric.stenseth@gmail.com>
2017-01-27 15:08:32 +0100

    adding identity server

--- src/Changelog/Startup.cs
+++ src/Changelog/Startup.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using IdentityServer4.Models;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,14 +18,19 @@ namespace Changelog
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddInMemoryClients(new List<Client>())
+                .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddTemporarySigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseIdentityServer();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            
         }
     }
 }
